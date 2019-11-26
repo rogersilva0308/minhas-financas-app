ea50a1a9942a223965aff4e0b35ebbdf41159cc4
Rogerio Ferreira da Silva <01444453106@goiasprev.go.gov.br>
2019-11-25 21:34:23 -0300

    adicionando a url da api remota

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:8080'
-    // baseURL: 'https://minhasfinancas-api.herokuapp.com'
+    baseURL: 'https://rogerio-api.herokuapp.com'
 })
 
 class ApiService {
